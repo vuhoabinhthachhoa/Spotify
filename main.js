5d4ee01592fd61955044b233536ae5084d4147c4
html <22120436@student.hcmus.edu.vn>
2024-01-03 16:33:38 +0700

    change the main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-// -----Render Song.-------
+// -----Render Song.-----
 songs = [
     {
         name: 'Khơi lòng',
